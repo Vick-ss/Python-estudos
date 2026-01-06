32a9d73ebbc328ab09f0362395c2204d4965f815
Vick <vicksantana517@gmail.com>
2026-01-06 20:58:36 -0300

    adicionando conceitos iniciais

---
+++ 01-Conceitos_iniciais.py
@@ -0,0 +1,59 @@
+print("Lan code")
+
+#não pode executar sem aspas
+#Tipos de dados>>> str- string  int -inteiro float-numeral com casa decimais 
+
+print(type(20.67))
+print(type("hii"))
+#type usado para mostrar o tipo de dados 
+
+print(30/10)
+print(30+20)
+print(50*5)
+print(2**4) 
+# ** quer dizer potencia
+print(10%2)
+#calcula o resto de uma divisão
+print("Olá, " + "Como vai?")
+#Concatenação pega duas strings e junta em uma só
+
+print(3/5) 
+#divisão
+
+#Variaveis --> começar com letras ou _ nãopode começar com numeros
+
+primeiro_numero= 7
+segundo_numero = 5
+#lemos como primeiro_numero recebe 7
+
+print(primeiro_numero + segundo_numero)
+print(primeiro_numero - segundo_numero)
+print(primeiro_numero * segundo_numero)
+print(primeiro_numero / segundo_numero)
+
+# variavel guarda na memoria 7 como 1num e 5 como 2num
+
+#comando input serve para pedir uma informação ao usuário
+
+nome = input ("Qual é o seu nome? ")
+print(nome)
+
+
+num1 = int(input("digite um número: "))
+num2 = int(input("Digite outro número: "))
+resultado_soma = num1 + num2
+print(f"o resultado da soma é {resultado_soma}")
+
+#necessário colocar o int para que seja reconhecido como numero int e nn uma string
+
+#Formatação da string de uma forma na qual conseguimos inserir variaveis usando (f ) e {variavel}
+
+print("Multiplicando Números")
+num1 = int(input("digite um número: "))
+num2 = int(input("Digite outro número: "))
+Resultado_Multiplicacao = num1 + num2
+
+print(f"o resultado da multiplicação entre {num1} e {num2} é: {Resultado_Multiplicacao}")
+
+#condicionais , dependendo de uma condigo ou regra 
+
