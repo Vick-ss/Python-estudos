77dabbc9f3e052cc1247419195112db2fe75b3d5
Vick <vicksantana517@gmail.com>
2026-01-14 22:53:31 -0300

    Realiza login no sistema de forma automática inserindo email e senha e clicando no botão de enter

---
+++ PowerUp/auxiliar.py
@@ -0,0 +1,8 @@
+import time 
+import pyautogui
+
+time.sleep(7)
+print (pyautogui.position())
+
+#espere 7s para determinar a posição 
+
--- PowerUp/codigo.py
+++ PowerUp/codigo.py
@@ -11,7 +11,7 @@ import time
 # pyautogui.press -> apertar 1 tecla
 # pyautogui.click -> clicar em algum lugar da tela
 # pyautogui.hotkey -> combinação de teclas
-pyautogui.PAUSE = 0.3
+pyautogui.PAUSE = 0.4
 
 # abrir o navegador (chrome)
 pyautogui.press("win")
@@ -21,5 +21,15 @@ pyautogui.press("enter")
 # entrar no link 
 pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 pyautogui.press("enter")
-time.sleep(3)
+time.sleep(4)
 
+# Passo 2: Fazer login
+# selecionar o campo de email
+
+pyautogui.click(x=395, y=410)
+# escrever o seu email
+pyautogui.write("pythonimpressionador@gmail.com")
+pyautogui.press("tab") # passando pro próximo campo
+pyautogui.write("Teste123Senha")
+pyautogui.press("tab")
+pyautogui.press("enter")
